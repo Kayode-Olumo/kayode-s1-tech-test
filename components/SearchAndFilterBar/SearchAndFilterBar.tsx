2202a9ba0f707dd9ccbab8ce63e3fa78e326719b
Kayode-Olumo <46356143+Kayode-Olumo@users.noreply.github.com>
2024-10-04 13:01:28 +0100

    text change

--- components/SearchAndFilterBar/SearchAndFilterBar.tsx
+++ components/SearchAndFilterBar/SearchAndFilterBar.tsx
@@ -22,7 +22,7 @@ const SearchAndFilterBar: FC<ISearchAndFilterBar> = ({
         <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 h-4 w-4 text-gray-400 dark:text-gray-500" />
         <Input
           type="text"
-          placeholder="Search for Stocks"
+          placeholder="Search for Stocks name or Company"
           value={searchQuery}
           onChange={(e) => setSearchQuery(e.target.value)}
           className="pl-10 pr-4 py-2 w-full bg-white dark:bg-gray-700 text-gray-900 dark:text-gray-100 border-gray-300 dark:border-gray-600 rounded-md"
